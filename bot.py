37ee509f6bf4888626377d7b1acd56820779d958
Spy <119489004+amthespy@users.noreply.github.com>
2023-07-07 21:37:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,13 +7,13 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 from pyrogram.errors import BadRequest
 
 # Configs
-API_HASH = os.environ['116795591c577f90e4519bdc265934b7
+API_HASH = os.environ['API_HASH', '116795591c577f90e4519bdc265934b7
 '] # Api hash
-APP_ID = int(os.environ['29569253']) # Api id/App id
-BOT_TOKEN = os.environ['6037473122:AAHGAL0iGxN2uLLPVA6KO8pUxLyqp6uRHaY'] # Bot token
-OWNER_ID = os.environ['5842877813'] # Your telegram id
-AS_ZIP = bool(os.environ['False']) # Upload method. If True: will Zip all your files and send as zipfile | If False: will send file one by one
-BUTTONS = bool(os.environ['Flase']) # Upload mode. If True: will send buttons (Zip or One by One) instead of AZ_ZIP | If False: will do as you've fill on AZ_ZIP
+APP_ID = int(os.environ['API_ID', '29569253']) # Api id/App id
+BOT_TOKEN = os.environ['BOT_TOKEN' '6037473122:AAHGAL0iGxN2uLLPVA6KO8pUxLyqp6uRHaY'] # Bot token
+OWNER_ID = os.environ['OWNER_ID', '5842877813'] # Your telegram id
+AS_ZIP = bool(os.environ['AS_ZIP', 'False']) # Upload method. If True: will Zip all your files and send as zipfile | If False: will send file one by one
+BUTTONS = bool(os.environ['BUTTONS', 'Flase']) # Upload mode. If True: will send buttons (Zip or One by One) instead of AZ_ZIP | If False: will do as you've fill on AZ_ZIP
 
 # Buttons
 START_BUTTONS=[
